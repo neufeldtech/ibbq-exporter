29be1630eca25e4cbc02ad30ecd446c1f8bebdee
Jordan Neufeld <jordan@neufeldtech.com>
2020-08-19 23:29:34 -0500

    init commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/neufeldtech/ibbq-exporter
+
+go 1.14
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-ble/ble v0.0.0-20200407180624-067514cd6e24
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mgutz/logxi v0.0.0-20161027140823-aebf8a7d67ab
+	github.com/sworisbreathing/go-ibbq/v2 v2.0.0-20200617033556-a8933daef274
+	golang.org/x/sys v0.0.0-20191220142924-d4481acd189f // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
---
+++ main.go
@@ -0,0 +1,74 @@
+/*
+   Copyright 2018 the original author or authors
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package main
+
+import (
+	"context"
+
+	glog "github.com/mgutz/logxi/v1"
+
+	"strconv"
+	"time"
+
+	"github.com/go-ble/ble"
+	"github.com/sworisbreathing/go-ibbq/v2"
+)
+
+var logger = glog.New("main")
+
+func temperatureReceived(temperatures []float64) {
+	logger.Info("Received temperature data", "temperatures", temperatures)
+}
+func batteryLevelReceived(batteryLevel int) {
+	logger.Info("Received battery data", "batteryPct", strconv.Itoa(batteryLevel))
+}
+func statusUpdated(status ibbq.Status) {
+	logger.Info("Status updated", "status", status)
+}
+
+func disconnectedHandler(cancel func(), done chan struct{}) func() {
+	return func() {
+		logger.Info("Disconnected")
+		cancel()
+		close(done)
+	}
+}
+
+func main() {
+	var err error
+	logger.Debug("initializing context")
+	ctx1, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	registerInterruptHandler(cancel)
+	ctx := ble.WithSigHandler(ctx1, cancel)
+	logger.Debug("context initialized")
+	var bbq ibbq.Ibbq
+	logger.Debug("instantiating ibbq struct")
+	done := make(chan struct{})
+	var config ibbq.Configuration
+	if config, err = ibbq.NewConfiguration(60*time.Second, 5*time.Minute); err != nil {
+		logger.Fatal("Error creating configuration", "err", err)
+	}
+	if bbq, err = ibbq.NewIbbq(ctx, config, disconnectedHandler(cancel, done), temperatureReceived, batteryLevelReceived, statusUpdated); err != nil {
+		logger.Fatal("Error creating iBBQ", "err", err)
+	}
+	logger.Debug("instantiated ibbq struct")
+	logger.Info("Connecting to device")
+	if err = bbq.Connect(); err != nil {
+		logger.Fatal("Error connecting to device", "err", err)
+	}
+	logger.Info("Connected to device")
+	<-ctx.Done()
+	<-done
+	logger.Info("Exiting")
+}
